30c9a73cd0d71df9a9f1c9f8f96e773529588b98
Alex Jungroth <aajungroth@ucdavis.edu>
2017-05-30 23:41:32 +0000

    The asteroids move in a random direction every second

--- client/watchout.js
+++ client/watchout.js
@@ -30,11 +30,14 @@ var testAsteroid = d3.select('.board')
 //d3.select("body").transition().duration(2050).style("background-color", "red");
 
 // asteroid = {x:1, y:1}
-var Asteroid = function() {
-  this.x = getRandomInt(100, 700);
-  this.y = getRandomInt(100, 450);
-  this.width = 50;
-  this.height = 50;
+var Asteroid = function(i) {
+
+  this.size = 25;
+  this.width = this.size;
+  this.height = this.size;
+  this.x = getRandomInt(0, 600);
+  this.y = getRandomInt(0, 350);
+
 };
 
 function getRandomInt(min, max) {
@@ -47,30 +50,65 @@ var makeAsteriods = function(numberOfAsteroids) {
   var asteroids = [];
 
   for (var i = 0; i < numberOfAsteroids; i++) {
-    asteroids.push(new Asteroid());
+    asteroids.push(new Asteroid(i));
   }
   return asteroids;
 };
 
-var data = makeAsteriods(20);
+var asteroids = makeAsteriods(25);
 
-console.log(data);
-
-d3.select(".board")
+var $enemies = d3.select(".board")
   .selectAll("img")
-  .data(makeAsteriods(20))
-  .enter()
+  .data(asteroids);
+
+$enemies.enter()
   .append('img')
   .attr('src', './asteroid.png')
-  .attr('top', function(asteroid){
-    return this.y;
-  })
-  .attr('left', function(asteroid){
-    return this.x;
+  .each(function(enemy){
+    var top = enemy.y + 'px';
+    var left = enemy.x + 'px';
+
+    d3.select(this).attr({
+      width: enemy.width,
+      height: enemy.height
+    });
+
+    d3.select(this).style({
+      position: 'absolute',
+      top : top,
+      left: left,
+    });
   });
+
+$enemies.exit().remove();
+
+var update = function() {
+   $enemies.transition()
+   .duration(1000)
+   .style('top', function(enemy){
+      var top = getRandomInt(0, 350);
+      return  top + 'px';
+   }).style('left', function(enemy){
+      var left = getRandomInt(0, 650);
+      return  left + 'px';
+   });
+};
+
   /*.style('transform', 'translate(100px, 100px)')
   .transition()
   .duration(5000)
   .style('transform', 'translate(400px, 400px)');*/
+
+console.log(d3);
+console.log(d3.timer);
+
+update();
+
+
+setInterval(function() {
+  console.log('timer');
+  asteroids = makeAsteriods(26);
+  update();
+}, 1000);
 
 console.log();
