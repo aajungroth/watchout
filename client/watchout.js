83b2aceb307d625e6902470b0fadf3324bdfdbe1
Alex Jungroth <aajungroth@ucdavis.edu>
2017-05-30 18:25:49 +0000

    Added an asteroid to the board

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,8 @@
 // start slingin' some d3 here.
+//testing comments
+//we are good
+var testAsteroid = d3.select('.board')
+  .append('img')
+  .attr('src', './asteroid.png');
+
+console.log(testAsteroid);
