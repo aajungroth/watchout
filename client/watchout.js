7874ff0a321c7c9c9f1c4ae1328212708e405e97
Alex Jungroth <aajungroth@ucdavis.edu>
2017-06-05 02:41:50 +0000

    Working collision detection

--- client/watchout.js
+++ client/watchout.js
@@ -1,11 +1,15 @@
+var mouse = {
+  x: window.innerWidth / 2,
+  y: window.innnerHeight / 2
+};
 var score = 0;
 var highScore = 0;
 var collisionCount = 0;
 
-var updateScore = function(){
+var updateScore = function() {
   d3.select('.scoreboard .current span').text(score);
-  d3.select('.scoreboard .highscore span').text(highscore);
-  d3.select('.scoreboard .collisionCount span').text(collisionCount);
+  d3.select('.scoreboard .highscore span').text(highScore);
+  d3.select('.scoreboard .collisions span').text(collisionCount);
 };
 
 var getRandomInt = function(min, max) {
@@ -19,7 +23,7 @@ var board = d3.select('.board').style({
   width: '700px'
 });
 
-var mouse = d3.select('.mouse').style({
+d3.select('.mouse').style({
   top: '225px',
   left: '350px',
   width: '30px',
@@ -67,8 +71,37 @@ var update = function(enemies) {
 update(enemies);
 
 var scoreTicker = function() {
-  score = score+1;
-  highscore = Math.max(score, highScore);
+  score = score + 1;
+  highScore = Math.max(score, highScore);
   updateScore();
 };
 setInterval(scoreTicker, 100);
+
+var prevCollision = false;
+
+var detectCollisions = function() {
+  var collision = false;
+
+  enemies.each(function() {
+    var cx = Math.round(Number(this.style.left.slice(0, this.style.left.length - 2))) + 15;
+    var cy = Math.round(Number(this.style.top.slice(0, this.style.left.length - 2))) + 15;
+    var x = cx - mouse.x;
+    var y = cy - mouse.y;
+    if (Math.sqrt(x * x + y * y) < 30 ) {
+      collision = true;
+    }
+  });
+
+  if (collision) {
+    board.style('background-color', 'red');
+    score = 0;
+    if (prevCollision !== collision) {
+      collisionCount++;
+    }
+  } else {
+    board.style('background-color', 'white');
+  }
+  prevCollision = collision;
+};
+
+d3.timer(detectCollisions);
